63defe72e7c82053bfd436a965b51fbc586bf944
Aotae <47459782+Aotae@users.noreply.github.com>
2022-03-04 14:05:33 -0800

    Update api.py
    brevet not Brevet

--- api/api.py
+++ api/api.py
@@ -11,10 +11,10 @@ API_PORT  = os.environ.get('API_PORT')
 
 app = Flask(__name__)
 myapi= Api(app)
-connect(host="mongodb://db:27017/Brevets")
+connect(host="mongodb://db:27017/brevets")
 
-myapi.add_resource(BrevetAPI,"/api/Brevet/<id>")
-myapi.add_resource(BrevetsAPI,"/api/Brevets")
+myapi.add_resource(BrevetAPI,"/api/brevet/<id>")
+myapi.add_resource(BrevetsAPI,"/api/brevets")
 
 if __name__ == "__main__":
     print("OPEN ON API", API_PORT)
